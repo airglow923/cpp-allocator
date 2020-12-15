f743494ece54b0b329907ea127d640d88fa5d2fd
airglow923 <p.hyundeok76@gmail.com>
2020-12-15 20:58:07 +0800

    add allocate_commons.cc

---
+++ src/hyundeok/allocator/allocator_commons.cc
@@ -0,0 +1,39 @@
+#include "hyundeok/allocator/allocator_commons.h"
+
+#include <unistd.h>
+
+#include <utility>
+
+namespace hyundeok::allocator {
+
+auto AlignHeap(SizeT n) -> WordT {
+  return (n + sizeof(WordT) - 1) & ~(sizeof(WordT) - 1);
+}
+
+/*
+ * Why not sizeof(char)?
+ *
+ * Because the size of a variable is not guaranteed to be the same as the size
+ * of an array with length 1.
+ */
+auto AllocateSize(SizeT size) -> SizeT {
+  return size + sizeof(HeapHeader) - sizeof(std::declval<HeapHeader>().data_);
+}
+
+auto RequestHeap(SizeT size) -> HeapHeader* {
+  auto* heap = static_cast<HeapHeader*>(sbrk(0));
+
+  if (sbrk(AllocateSize(size)) == reinterpret_cast<void*>(-1))
+    heap = nullptr;
+
+  return heap;
+}
+
+auto ConvertPtrToVoidPtr(void* ptr) -> void* { return ptr; }
+
+auto GetHeapHeader(void* heap) -> HeapHeader* {
+  return static_cast<HeapHeader*>(ConvertPtrToVoidPtr(
+      static_cast<char*>(heap) - sizeof(HeapHeader) + sizeof(WordT)));
+}
+
+} // namespace hyundeok::allocator
