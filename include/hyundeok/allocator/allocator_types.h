ec6d8754e98ae79a9693f6883fcbf545b34342a4
airglow923 <p.hyundeok76@gmail.com>
2020-12-18 22:07:12 +0800

    add static constant denoting max pointer address to allocator_types.h

--- include/hyundeok/allocator/allocator_types.h
+++ include/hyundeok/allocator/allocator_types.h
@@ -6,8 +6,29 @@
 namespace hyundeok {
 namespace allocator {
 
+namespace {
+
+using LL = signed long long int;
+
+template <LL N, LL X>
+struct PowerOf {
+  static constexpr LL value_ = N * PowerOf<N, X - 1>::value_;
+};
+
+template <LL N>
+struct PowerOf<N, 0> {
+  static constexpr LL value_ = 1;
+};
+
+template <LL N, LL X>
+inline constexpr LL PowerOfV = PowerOf<N, X>::value_;
+
+} // namespace
+
 using WordT = std::intptr_t;
 using SizeT = std::size_t;
+
+static inline constexpr auto kMaxPtrAddress = PowerOfV<2, sizeof(WordT)> - 1;
 
 /*
  * next_ points to the first address of the next heap block.
