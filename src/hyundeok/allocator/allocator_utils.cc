a218a94e517ead14a8be2117415c35859f01fef2
airglow923 <p.hyundeok76@gmail.com>
2020-12-31 00:00:53 +0800

    implement heap splitting && remove redundant function && add more functions

--- src/hyundeok/allocator/allocator_utils.cc
+++ src/hyundeok/allocator/allocator_utils.cc
@@ -46,11 +46,26 @@ auto RequestHeap(SizeT size) -> HeapHeader* {
   return heap;
 }
 
-auto ConvertPtrToVoidPtr(void* ptr) -> void* { return ptr; }
+auto ConvertPtrToHeapHeader(void* ptr) -> HeapHeader* {
+  return static_cast<HeapHeader*>(ptr);
+}
 
 auto GetHeapHeader(void* heap) -> HeapHeader* {
-  return static_cast<HeapHeader*>(ConvertPtrToVoidPtr(
-      static_cast<char*>(heap) - sizeof(HeapHeader) + sizeof(WordT)));
+  return ConvertPtrToHeapHeader(static_cast<char*>(heap) - sizeof(HeapHeader) +
+                                sizeof(WordT));
+}
+
+auto InitializeHeapHeader(HeapHeader* heap, SizeT size) -> void {
+  heap->size_ = size;
+  heap->used_ = false;
+  heap->next_ = nullptr;
+}
+
+auto SplitHeap(HeapHeader* heap, SizeT size) -> HeapHeader* {
+  heap->size_ -= AllocateSize(size);
+  HeapHeader* new_heap = ConvertPtrToHeapHeader(heap->data_ + heap->size_);
+  InitializeHeapHeader(heap, size);
+  return new_heap;
 }
 
 } // namespace hyundeok::allocator
