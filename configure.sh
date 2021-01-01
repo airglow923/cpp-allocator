cd0b2fd12597dcb4f8d3ace762d8e4613b17bc0b
airglow923 <p.hyundeok76@gmail.com>
2021-01-01 10:32:13 +0800

    fix #24: use which to find executable on PATH in configure.sh

--- configure.sh
+++ configure.sh
@@ -11,21 +11,24 @@ if [ -d "$1" ]; then
   CMAKE_DIR="$1"
 fi
 
-if [ -f /bin/clang++ ]; then
-  CLANG_VERSION=`clang --version | grep clang | sed -E 's/.* (.*)-.*$/\1/'`
-  CLANG_VERSION_MAJOR=`echo $CLANG_VERSION | sed -E 's/([^.]+).*/\1/'`
+CLANG_PATH=$(which clang++)
+
+if [ -x "$CLANG_PATH" ]; then
+  CLANG_VERSION=$(clang --version | grep clang | sed -E 's/.* (.*)-.*$/\1/')
+  CLANG_VERSION_MAJOR=$(echo $CLANG_VERSION | sed -E 's/([^.]+).*/\1/')
   if [ "$CLANG_VERSION_MAJOR" -ge "$CLANG_REQUIRED_VERSION" ]; then
     CMAKE_CXX_COMPILER=clang++
   fi
 fi
 
 if [ ! "$CMAKE_CXX_COMPILER" ]; then
-  GCC_VERSION=`gcc --version | grep ^gcc | sed 's/^.* //'`
-  GCC_VERSION_MAJOR=`echo $GCC_VERSION | sed -E 's/([^.]+).*/\1/'`
+  GCC_VERSION=$(gcc --version | grep ^gcc | sed 's/^.* //')
+  GCC_VERSION_MAJOR=$(echo $GCC_VERSION | sed -E 's/([^.]+).*/\1/')
   if [ "$GCC_VERSION_MAJOR" -ge "$GCC_REQUIRED_VERSION" ]; then
     CMAKE_CXX_COMPILER=g++
   else
-    if [ -f /bin/g++-10 ]; then
+    GCC_10_PATH=$(which g++-10)
+    if [ -x "$GCC_10_PATH" ]; then
       CMAKE_CXX_COMPILER=g++-10
     else
       echo "Cannot find a compatible compiler. Terminate build."
