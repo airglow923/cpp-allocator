ff4e2ad69f15b74997e3b97c7c105508543628fa
airglow923 <p.hyundeok76@gmail.com>
2021-01-03 10:10:56 +0800

    edit free_list.cc

--- src/hyundeok/allocator/linked_list/free_list.cc
+++ src/hyundeok/allocator/linked_list/free_list.cc
@@ -10,26 +10,34 @@ auto GetFreeListHead() -> HeapHeader*& {
 }
 
 auto AddFreeListNode(HeapHeader* node) -> void {
-  auto*& iter = GetFreeListHead();
+  auto*& head = GetFreeListHead();
 
-  if (iter == nullptr) {
-    iter = node;
-    node->next_ = iter;
+  if (head == nullptr) {
+    head = node;
+    node->next_ = head;
     return;
   }
 
+  // This additional variable prevents the head from being assigned to by
+  // the pointer "next_".
+  auto* iter = head;
+
+  // If the below for loop uses the reference to head,
+  //    head = head->next_;
+  // this would make GetFreeListHead() point to head->next_;
   for (; iter->next_ != nullptr; iter = iter->next_) continue;
-  iter = node;
+  iter->next_ = node;
 }
 
 auto RemoveFreeListNode(HeapHeader* node) -> void {
-  auto*& iter = GetFreeListHead();
+  auto*& head = GetFreeListHead();
 
-  if (iter == nullptr) return;
+  if (head == nullptr) return;
 
-  if (iter == node) {
-    iter = nullptr;
+  if (head == node) {
+    head = head->next_;
   } else {
+    auto* iter = head;
     for (; iter->next_ != node; iter = iter->next_) continue;
     iter->next_ = node->next_;
   }
